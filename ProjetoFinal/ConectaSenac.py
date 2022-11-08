d465a6e28d996219dd5bd5ab156cde707e28bd81
Dan <dan@outlook.com.br>
2022-11-08 16:03:07 -0300

    Link para esqueci a senha (de novo)

--- ProjetoFinal/ConectaSenac.py
+++ ProjetoFinal/ConectaSenac.py
@@ -52,7 +52,7 @@ class Inicio:
         # self.botao_cadastrar.bind('<Any-Button>') receberá a função para cadastrar futuramente
 
         self.botao_esqueci_senha = tk.Button(self.janela_login, text='Esqueci minha senha', font='Verdana 16',
-                                             fg='#004AAD', bg='#F5F5F5', relief=FLAT, command=lambda : self.esqueciSenha())
+                                             fg='#004AAD', bg='#F5F5F5', relief=FLAT, command=lambda : self.tela_senha())
         self.botao_esqueci_senha.place(relx=0.43, rely=0.83, relheight=0.05, relwidth=0.20)
         # self.label_esqueci_senha.bind('<Return>') receberá a função para cadastrar futuramente
         # self.botao_esqueci_senha.bind('<Any-Button>') receberá a função para cadastrar futuramente
@@ -102,6 +102,50 @@ class Inicio:
         self.label_inferior.place(relx=0, relwidth=1, relheight=0.08, rely=0.92)
 
         self.inicial.mainloop()
+
+    def tela_senha(self):
+        class Janela_Recuperar_Senha:
+            def __init__(self, janela):
+                self.janela_recuperar_senha = janela
+                self.janela_recuperar_senha.title('Recuperação de Senha')  # Trocar o título da janela
+                self.janela_recuperar_senha.iconbitmap('icone.ico')  # Verificar se tem o ícone no seu arquivo
+                self.janela_recuperar_senha.geometry('690x550+300+10')
+                self.janela_recuperar_senha['bg'] = '#F5F5F5'
+                self.janela_recuperar_senha.resizable(width=False, height=False)
+
+                self.label_superior = tk.Label(self.janela_recuperar_senha, bg='#004AAD')
+                self.label_superior.place(relx=0, relwidth=1, relheight=0.08, rely=0)
+
+                self.senac_logo = tk.PhotoImage(file=r'..\ProjetoFinal\logo_simbolo.png')
+                self.label_senac_logo = tk.Label(self.janela_recuperar_senha, image=self.senac_logo, bg='#F5F5F5')
+                self.label_senac_logo.place(relx=0.02, rely=0.09)
+
+                self.label_Cadastro_usuario = tk.Label(self.janela_recuperar_senha, text='Recuperar Senha',
+                                                       bg='#F5F5F5',
+                                                       font='Inter 24 bold')
+                self.label_Cadastro_usuario.place(relx=0.15, relwidth=0.40, rely=0.08, relheight=0.15)
+
+                self.label_nome = tk.Label(self.janela_recuperar_senha, text='CPF:', font='Inter 17 bold', bg='#F5F5F5')
+                self.label_nome.place(relx=0.15, relwidth=0.10, rely=0.35, relheight=0.05)
+                self.entrada_nome = tk.Entry(self.janela_recuperar_senha, font='Inter 17', bg='#D9D9D9')
+                self.entrada_nome.place(relx=0.28, relwidth=0.50, rely=0.35, relheight=0.05)
+
+                self.label_email = tk.Label(self.janela_recuperar_senha, text='E-mail:', font='Inter 17 bold',
+                                            bg='#F5F5F5')
+                self.label_email.place(relx=0.15, relwidth=0.12, rely=0.45, relheight=0.05)
+                self.entrada_email = tk.Entry(self.janela_recuperar_senha, font='Inter 17', bg='#D9D9D9')
+                self.entrada_email.place(relx=0.28, relwidth=0.50, rely=0.45, relheight=0.05)
+
+                self.botao_cadastrar = tk.Button(self.janela_recuperar_senha, text='Enviar', font='Inter 17 bold',
+                                                 fg='white', bg='#004AAD')
+                self.botao_cadastrar.place(relx=0.43, relwidth=0.20, rely=0.60, relheight=0.10)
+
+                self.label_inferior = tk.Label(self.janela_recuperar_senha, bg='#004AAD')
+                self.label_inferior.place(relx=0, relwidth=1, relheight=0.08, rely=0.92)
+
+        janela = Tk()
+        objetoJanela = Janela_Recuperar_Senha(janela)
+        janela.mainloop()
 
     def login_inicial(self):
         c = self.conexao.cursor()
--- ProjetoFinal/telaRecuperarSenha.py
+++ ProjetoFinal/telaRecuperarSenha.py
@@ -1,43 +1,44 @@
 from tkinter import *
 import tkinter as tk
 
-class Janela_Cadastrar_Usuario:
-    def __init__(self, janela):
-        self.janela_cadastrar_usuario = janela
-        self.janela_cadastrar_usuario.title('Recuperação de Senha')  # Trocar o título da janela
-        # self.janela08.iconbitmap('icone.ico')  # Verificar se tem o ícone no seu arquivo
-        self.janela_cadastrar_usuario.geometry('690x550+300+10')
-        self.janela_cadastrar_usuario['bg'] = '#F5F5F5'
-        self.janela_cadastrar_usuario.resizable(width=False, height=False)
+def tela_senha():
+    class Janela_Recuperar_Senha:
+        def __init__(self, janela):
+            self.janela_recuperar_senha = janela
+            self.janela_recuperar_senha.title('Recuperação de Senha')  # Trocar o título da janela
+            # self.janela08.iconbitmap('icone.ico')  # Verificar se tem o ícone no seu arquivo
+            self.janela_recuperar_senha.geometry('690x550+300+10')
+            self.janela_recuperar_senha['bg'] = '#F5F5F5'
+            self.janela_recuperar_senha.resizable(width=False, height=False)
 
-        self.label_superior = tk.Label(self.janela_cadastrar_usuario, bg='#004AAD')
-        self.label_superior.place(relx=0, relwidth=1, relheight=0.08, rely=0)
+            self.label_superior = tk.Label(self.janela_recuperar_senha, bg='#004AAD')
+            self.label_superior.place(relx=0, relwidth=1, relheight=0.08, rely=0)
 
-        self.senac_logo = tk.PhotoImage(file='logo_simbolo.png')
-        self.label_senac_logo = tk.Label(self.janela_cadastrar_usuario, image=self.senac_logo, bg='#F5F5F5')
-        self.label_senac_logo.place(relx=0.02, rely=0.09)
+            self.senac_logo = tk.PhotoImage(file='logo_simbolo.png')
+            self.label_senac_logo = tk.Label(self.janela_recuperar_senha, image=self.senac_logo, bg='#F5F5F5')
+            self.label_senac_logo.place(relx=0.02, rely=0.09)
 
-        self.label_Cadastro_usuario = tk.Label(self.janela_cadastrar_usuario, text='Recuperar Senha', bg='#F5F5F5',
-                                               font='Inter 24 bold')
-        self.label_Cadastro_usuario.place(relx=0.15, relwidth=0.40, rely=0.08, relheight=0.15)
+            self.label_Cadastro_usuario = tk.Label(self.janela_recuperar_senha, text='Recuperar Senha', bg='#F5F5F5',
+                                                   font='Inter 24 bold')
+            self.label_Cadastro_usuario.place(relx=0.15, relwidth=0.40, rely=0.08, relheight=0.15)
 
-        self.label_nome = tk.Label(self.janela_cadastrar_usuario, text='CPF:', font='Inter 17 bold', bg='#F5F5F5')
-        self.label_nome.place(relx=0.15, relwidth=0.10, rely=0.35, relheight=0.05)
-        self.entrada_nome = tk.Entry(self.janela_cadastrar_usuario, font='Inter 17', bg='#D9D9D9')
-        self.entrada_nome.place(relx=0.28, relwidth=0.50, rely=0.35, relheight=0.05)
+            self.label_nome = tk.Label(self.janela_recuperar_senha, text='CPF:', font='Inter 17 bold', bg='#F5F5F5')
+            self.label_nome.place(relx=0.15, relwidth=0.10, rely=0.35, relheight=0.05)
+            self.entrada_nome = tk.Entry(self.janela_recuperar_senha, font='Inter 17', bg='#D9D9D9')
+            self.entrada_nome.place(relx=0.28, relwidth=0.50, rely=0.35, relheight=0.05)
 
-        self.label_email = tk.Label(self.janela_cadastrar_usuario, text='E-mail:', font='Inter 17 bold', bg='#F5F5F5')
-        self.label_email.place(relx=0.15, relwidth=0.12, rely=0.45, relheight=0.05)
-        self.entrada_email = tk.Entry(self.janela_cadastrar_usuario, font='Inter 17',  bg='#D9D9D9')
-        self.entrada_email.place(relx=0.28, relwidth=0.50, rely=0.45, relheight=0.05)
+            self.label_email = tk.Label(self.janela_recuperar_senha, text='E-mail:', font='Inter 17 bold', bg='#F5F5F5')
+            self.label_email.place(relx=0.15, relwidth=0.12, rely=0.45, relheight=0.05)
+            self.entrada_email = tk.Entry(self.janela_recuperar_senha, font='Inter 17', bg='#D9D9D9')
+            self.entrada_email.place(relx=0.28, relwidth=0.50, rely=0.45, relheight=0.05)
 
-        self.botao_cadastrar = tk.Button(self.janela_cadastrar_usuario, text='Enviar', font='Inter 17 bold', fg='white', bg='#004AAD')
-        self.botao_cadastrar.place(relx=0.43, relwidth=0.20, rely=0.60, relheight=0.10)
+            self.botao_cadastrar = tk.Button(self.janela_recuperar_senha, text='Enviar', font='Inter 17 bold', fg='white', bg='#004AAD')
+            self.botao_cadastrar.place(relx=0.43, relwidth=0.20, rely=0.60, relheight=0.10)
 
-        self.label_inferior = tk.Label(self.janela_cadastrar_usuario, bg='#004AAD')
-        self.label_inferior.place(relx=0, relwidth=1, relheight=0.08, rely=0.92)
+            self.label_inferior = tk.Label(self.janela_recuperar_senha, bg='#004AAD')
+            self.label_inferior.place(relx=0, relwidth=1, relheight=0.08, rely=0.92)
 
 
-janela = Tk()
-objetoJanela = Janela_Cadastrar_Usuario(janela)
-janela.mainloop()
+    janela = Tk()
+    objetoJanela = Janela_Recuperar_Senha(janela)
+    janela.mainloop()
